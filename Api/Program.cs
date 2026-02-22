a8a5c53da7a9e59c876e9ed6550efd808682151c
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-02-23 01:45:29 +0300

    configure EF Core, connection string, and automatic migrations

---
+++ Api/Program.cs
@@ -0,0 +1,41 @@
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// To provide auto migration, we need to add the DbContext to the services container.
+builder.Services.AddDbContext<RealTimeChatAppContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+
+// To provide auto migration, we need to create a scope and get the DbContext from the service provider, then call the Migrate method on the database. This will apply any pending migrations to the database when the application starts.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<RealTimeChatAppContext>();
+    db.Database.Migrate();
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ DataAccess/Concrete/EntityFramework/Contexts/RealTimeChatAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class RealTimeChatAppContext : DbContext
+    {
+        public RealTimeChatAppContext(DbContextOptions<RealTimeChatAppContext> options) : base(options)
+        {
+        }
+
+        // DbSet properties for your entities go here
+        // Those are not ready yet, so they are commented out for now.
+    }
+}
