63cffebb41ce449e8797ce5f0637291b0028eace
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-02-23 03:18:52 +0300

    feat(core): introduce IEntity and IEntityRepository abstractions

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<T?> GetAsync(Expression<Func<T, bool>> filter, CancellationToken ct = default);
+        Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, CancellationToken ct = default);
+
+        Task AddSync(T entity, CancellationToken ct = default);
+        Task UpdateAsync(T entity, CancellationToken ct = default);
+        Task DeleteAsync(T entity, CancellationToken ct = default);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,49 @@
+﻿using Core.DataAccess;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<TContext, TEntity> : IEntityRepository<TEntity>
+        where TContext : DbContext, new()
+        where TEntity : class, IEntity, new()
+    {
+        private readonly TContext context;
+        public EfEntityRepositoryBase(TContext _context)
+        {
+            context = _context;
+        }
+
+
+        public async Task AddSync(TEntity entity, CancellationToken ct = default)
+        {
+            await context.Set<TEntity>().AddAsync(entity, ct);
+            await context.SaveChangesAsync(ct);
+        }
+
+        public async Task DeleteAsync(TEntity entity, CancellationToken ct = default)
+        {
+            context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync(ct);
+        }
+
+        public async Task UpdateAsync(TEntity entity, CancellationToken ct = default)
+        {
+            context.Set<TEntity>().Update(entity);
+            await context.SaveChangesAsync(ct);
+        }
+
+        public async Task<IReadOnlyCollection<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? filter = null, CancellationToken ct = default)
+        {
+            return filter == null
+                ? await context.Set<TEntity>().ToListAsync(ct)
+                : await context.Set<TEntity>().Where(filter).ToListAsync(ct);
+        }
+
+        public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> filter, CancellationToken ct = default)
+        {
+            return await context.Set<TEntity>().FirstOrDefaultAsync(filter, ct);
+        }
+    }
+}
