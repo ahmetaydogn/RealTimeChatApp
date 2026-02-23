d530cc336588a4fd87ef11d0d37e52149edb3d4d
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-02-23 15:54:38 +0300

    BaseEntity added

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
