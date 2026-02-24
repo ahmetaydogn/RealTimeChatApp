ae2aa9f66f0be024a2c14ef196f68aa76f3302fe
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-02-24 23:18:42 +0300

    feat: add AuthProvider with JWT token generation and some updates

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService _authService)
+        {
+            authService = _authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var token = await authService.Register(dto);
+            if (token == null)
+            {
+                return BadRequest("Kullanıcı oluşturulamadı.");
+            }
+
+            return Ok(new { token });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var token = await authService.Login(dto);
+            if (token == null)
+            {
+                return BadRequest("Geçersiz kullanıcı adı veya şifre.");
+            }
+
+            return Ok(new { token });
+        }
+    }
+}
