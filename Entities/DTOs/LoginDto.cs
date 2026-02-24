23ec04bfdef122076790ed1a870947cfb198dd5b
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-02-24 14:02:18 +0300

    feat(dto): add DTO models for authentication and chat operations

---
+++ Entities/DTOs/LoginDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class LoginDto : IEntityDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
