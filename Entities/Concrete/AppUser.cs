38902e48fa4dc64ca6823a0fa4e8c3a57c48eb2b
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-02-24 23:21:29 +0300

    feat: some updates in AppUser

--- Entities/Concrete/AppUser.cs
+++ Entities/Concrete/AppUser.cs
@@ -5,14 +5,14 @@ namespace Entities.Concrete
 {
     public class AppUser : BaseEntity
     {
-        public string Username { get; set; } = null!;
-        public string DisplayName { get; set; } = null!;
-        public string Email { get; set; }
-        public string PasswordHash { get; set; } = null!;
+        public string? Username { get; set; } = null!;
+        public string? DisplayName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? PasswordHash { get; set; } = null!;
 
         public UserType UserType { get; set; } = UserType.Normal;
 
-        public ICollection<RoomMember> RoomMembers { get; set; } = new List<RoomMember>();
-        public ICollection<Message> Messages { get; set; } = new List<Message>();
+        public ICollection<RoomMember>? RoomMembers { get; set; } = new List<RoomMember>();
+        public ICollection<Message>? Messages { get; set; } = new List<Message>();
     }
 }
