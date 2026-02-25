3be07d3bffa44cfac806e5f5f8bda71e9676c1c4
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-02-26 01:40:11 +0300

    feat: complete EfRoomDal async CRUD implementation

---
+++ DataAccess/Concrete/EntityFramework/EfRoomDal.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRoomDal : EfEntityRepositoryBase<RealTimeChatAppContext, Room>, IRoomDal
+    {
+        public EfRoomDal(RealTimeChatAppContext _context) : base(_context)
+        {
+        }
+    }
+}
