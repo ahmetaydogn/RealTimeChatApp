7b5aa00cfb0389ca5a9b73afc30724ab0d3f994a
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-03-01 14:38:36 +0300

    feat: add initial database migration with AppUsers table

--- DataAccess/Migrations/20260301110233_AddRoomAndMessageTable.Designer.cs
+++ DataAccess/Migrations/20260301110233_AddRoomAndMessageTable.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess.Concrete.EntityFramework.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(RealTimeChatAppContext))]
-    partial class RealTimeChatAppContextModelSnapshot : ModelSnapshot
+    [Migration("20260301110233_AddRoomAndMessageTable")]
+    partial class AddRoomAndMessageTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20260301110233_AddRoomAndMessageTable.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoomAndMessageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AppUsers_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoomMembers_AppUsers_UserId",
+                table: "RoomMembers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_RoomName",
+                table: "Rooms");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoomName",
+                table: "Rooms",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Messages",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AppUserId",
+                table: "Messages",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_AppUserId",
+                table: "Messages",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AppUsers_AppUserId",
+                table: "Messages",
+                column: "AppUserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AppUsers_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoomMembers_AppUsers_UserId",
+                table: "RoomMembers",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AppUsers_AppUserId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AppUsers_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoomMembers_AppUsers_UserId",
+                table: "RoomMembers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_AppUserId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoomName",
+                table: "Rooms",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Messages",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_RoomName",
+                table: "Rooms",
+                column: "RoomName");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AppUsers_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoomMembers_AppUsers_UserId",
+                table: "RoomMembers",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
