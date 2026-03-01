417053664c3737787aa4e47c7463c571f72f4633
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-03-01 14:37:27 +0300

    feat: add RoomConfiguration for EF Core

--- DataAccess/Concrete/EntityFramework/Configurations/RoomConfiguration.cs
+++ DataAccess/Concrete/EntityFramework/Configurations/RoomConfiguration.cs
@@ -4,17 +4,24 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DataAccess.Concrete.EntityFramework.Configurations
 {
-    internal class RoomConfiguration : IEntityTypeConfiguration<Room>
+    public class RoomConfiguration : IEntityTypeConfiguration<Room>
     {
         public void Configure(EntityTypeBuilder<Room> builder)
         {
             builder.HasKey(x => x.Id);
 
-            builder.HasIndex(x => x.RoomName);
+            builder.Property(x => x.RoomName)
+                .IsRequired()
+                .HasMaxLength(100);
 
             builder.HasMany(x => x.Messages)
-                .WithOne(m => m.Room)
-                .HasForeignKey(m => m.RoomId)
+                .WithOne(x => x.Room)
+                .HasForeignKey(x => x.RoomId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Members)
+                .WithOne(x => x.Room)
+                .HasForeignKey(x => x.RoomId)
                 .OnDelete(DeleteBehavior.Cascade);
         }
     }
