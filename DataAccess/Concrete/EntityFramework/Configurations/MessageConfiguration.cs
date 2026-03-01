6c2d20a38c0b31ff68f36cda46f7d12b0107395d
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-03-01 14:37:22 +0300

    feat: add MessageConfiguration for EF Core

--- DataAccess/Concrete/EntityFramework/Configurations/MessageConfiguration.cs
+++ DataAccess/Concrete/EntityFramework/Configurations/MessageConfiguration.cs
@@ -8,11 +8,17 @@ namespace DataAccess.Concrete.EntityFramework.Configurations
     {
         public void Configure(EntityTypeBuilder<Message> builder)
         {
+
             builder.HasKey(x => x.Id);
 
             builder.Property(x => x.Content)
                 .IsRequired()
-                .HasMaxLength(1000);
+                .HasMaxLength(2000);
+
+            builder.HasOne(x => x.Sender)
+                .WithMany()
+                .HasForeignKey(x => x.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             builder.HasIndex(x => new { x.RoomId, x.CreatedAt });
         }
