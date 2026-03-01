529fc7cf77546dc724f695a4f5e392c1092dae0d
ahmetaydogn <aydogan60ahmet@gmail.com>
2026-03-01 14:37:16 +0300

    feat: add EfMessageDal implementation

---
+++ DataAccess/Concrete/EntityFramework/EfMessageDal.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfMessageDal : EfEntityRepositoryBase<RealTimeChatAppContext, Message>, IMessageDal
+    {
+        public EfMessageDal(RealTimeChatAppContext _context) : base(_context)
+        {
+        }
+    }
+}
